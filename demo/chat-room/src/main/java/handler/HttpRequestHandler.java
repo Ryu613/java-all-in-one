1ab39ad58d96c6d9675d779df4a42c0239334357
Ryu613 <411379453@qq.com>
2023-01-11 17:21:12 +0800

    updated demo

--- demo/chat-room/src/main/java/handler/HttpRequestHandler.java
+++ demo/chat-room/src/main/java/handler/HttpRequestHandler.java
@@ -1,8 +1,9 @@
 package handler;
 
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.*;
 import io.netty.handler.codec.http.*;
+import io.netty.handler.ssl.SslHandler;
+import io.netty.handler.stream.ChunkedNioFile;
 
 import java.io.File;
 import java.io.RandomAccessFile;
@@ -42,7 +43,42 @@ public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequ
             RandomAccessFile file = new RandomAccessFile(INDEX, "r");
             HttpResponse response = new DefaultHttpResponse(
                     request.getProtocolVersion(), HttpResponseStatus.OK);
-
+            response.headers().set(
+                    HttpHeaders.Names.CONTENT_TYPE,
+                    "text/plain; charset=UTF-8");
+            boolean keepAlive = HttpHeaders.isKeepAlive(request);
+            if(keepAlive) {
+                response.headers().set(
+                        HttpHeaders.Names.CONTENT_LENGTH, file.length()
+                );
+                response.headers().set(
+                        HttpHeaders.Names.CONNECTION,
+                        HttpHeaders.Values.KEEP_ALIVE
+                );
+                ctx.write(response);
+                if(ctx.pipeline().get(SslHandler.class) == null) {
+                    ctx.write(new DefaultFileRegion(file.getChannel(), 0, file.length()));
+                } else {
+                    ctx.write(new ChunkedNioFile(file.getChannel()));
+                }
+                ChannelFuture future = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+                if(!keepAlive) {
+                    future.addListener(ChannelFutureListener.CLOSE);
+                }
+            }
         }
+    }
+
+    private static void send100Continue(ChannelHandlerContext ctx) {
+        FullHttpResponse response = new DefaultFullHttpResponse(
+                HttpVersion.HTTP_1_1, HttpResponseStatus.CONTINUE
+        );
+        ctx.writeAndFlush(response);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
     }
 }
